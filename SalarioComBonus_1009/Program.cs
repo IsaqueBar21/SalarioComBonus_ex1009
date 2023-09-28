2f2a0d60748be7d2a6617934e4d085e40aa1c1c1
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-09-28 09:04:18 -0300

    primeiro commit

---
+++ SalarioComBonus_1009/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string nome;
+        double salario, comissao;
+
+        Console.WriteLine("Escreva o nome do funcionário: ");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Qual o valor do salário do funcionário?: ");
+        salario = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual o valor das vendas do funcionário?:  ");
+        comissao = double.Parse(Console.ReadLine());
+
+        comissao = (comissao / 100) * 15;
+        double total = comissao + salario;
+
+        Console.WriteLine("Total a receber = R$ " + total.ToString("f2", CultureInfo.InvariantCulture));
+
+    }
+
+}
